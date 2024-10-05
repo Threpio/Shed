50c45a417b14459ca4fba78eb5c682e7e1936c5f
Theo Andresier <49570057+Threpio@users.noreply.github.com>
2024-10-05 22:46:53 +0200

    Added wails project and removed gin

--- main.go
+++ main.go
@@ -1,48 +1,58 @@
 package main
 
 import (
-	"context"
-	"database/sql"
+	"embed"
 	_ "embed"
-
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/threpio/shed/data"
-
-	"github.com/gin-gonic/gin"
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
 //go:embed schema.sql
 var ddl string
 
-func run() error {
-	ctx := context.Background()
+//go:embed all:frontend/dist
+var assets embed.FS
 
-	db, err := sql.Open("sqlite3", ":memory:")
-	if err != nil {
-		return err
-	}
-
-	// create tables
-	if _, err := db.ExecContext(ctx, ddl); err != nil {
-		return err
-	}
-
-	_ = data.New(db)
-
-	return nil
-}
+//func run() error {
+//	ctx := context.Background()
+//
+//	db, err := sql.Open("sqlite3", ":memory:")
+//	if err != nil {
+//		return err
+//	}
+//
+//	// create tables
+//	if _, err := db.ExecContext(ctx, ddl); err != nil {
+//		return err
+//	}
+//
+//	_ = data.New(db)
+//
+//	return nil
+//}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "shed",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
 }
-
-/*
-/healthz
-
-*/
